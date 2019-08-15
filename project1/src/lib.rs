12069c2f0a8c32522a22be246e88f20cf5f72ca1
Sighery <sighery7@gmail.com>
2019-08-15 23:10:19 +0200

    Store values in memory
    This completes part 4 of project 1:
    https://github.com/pingcap/talent-plan/blob/master/rust/projects/project-1/project.md#part-4-store-values-in-memory
    
    This commit basically implements the functionality of the key-value
    store, storing values in memory. With this, the rest of the tests pass
    (although now certain cli tests that relied on those methods not being
    implemented fail).

--- project1/src/lib.rs
+++ project1/src/lib.rs
@@ -1,30 +1,27 @@
 use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Default, Debug)]
 pub struct KvStore {
 	storage: HashMap<String, String>,
 }
 
 impl KvStore {
-	pub fn new() -> KvStore {
-		KvStore { storage: HashMap::new() }
+	pub fn new() -> Self {
+		Self { storage: HashMap::new() }
 	}
 
 	pub fn set(&mut self, key: String, value: String) {
-		// self.storage.insert(key, value);
-		panic!("unimplemented");
+		self.storage.insert(key, value);
 	}
 
 	pub fn get(&mut self, key: String) -> Option<String> {
-		// match self.storage.get(&key) {
-		// 	None => None,
-		// 	Some(value) => Some(value.clone()),
-		// }
-		panic!("unimplemented");
+		match self.storage.get(&key) {
+			None => None,
+			Some(value) => Some(value.clone()),
+		}
 	}
 
 	pub fn remove(&mut self, key: String) {
-		// self.storage.remove(&key);
-		panic!("unimplemented");
+		self.storage.remove(&key);
 	}
 }
