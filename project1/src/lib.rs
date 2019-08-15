6296064a6bf3bfdc2c9829877fb9257e9ce70aa8
Sighery <sighery7@gmail.com>
2019-08-16 00:36:09 +0200

    Add documentation
    This completes part 5 of project 1:
    https://github.com/pingcap/talent-plan/blob/master/rust/projects/project-1/project.md#part-5-documentation
    
    This commit add documentation for the kvs library, and some examples
    that can be run with `cargo test --doc` for each public method.

--- project1/src/lib.rs
+++ project1/src/lib.rs
@@ -1,19 +1,41 @@
+#![deny(missing_docs)]
+//! A key value store module
+
 use std::collections::HashMap;
 
 #[derive(Default, Debug)]
+/// Struct for a key-value store of String keys and values
 pub struct KvStore {
 	storage: HashMap<String, String>,
 }
 
 impl KvStore {
+	/// Constructs a new `KvStore`
+	///
+	/// # Examples
+	///
+	/// ```
+	/// use kvs::KvStore;
+	///
+	/// let mut kvstore = KvStore::new();
+	/// ```
 	pub fn new() -> Self {
 		Self { storage: HashMap::new() }
 	}
 
-	pub fn set(&mut self, key: String, value: String) {
-		self.storage.insert(key, value);
-	}
-
+	/// Retrieve a value from the store, from a key String
+	///
+	/// # Examples
+	///
+	/// ```
+	/// use kvs::KvStore;
+	///
+	/// let mut kvstore = KvStore::new();
+	/// match kvstore.get("test_key".to_string()) {
+	/// 	Some(value) => println!("Retrieved value {}", value),
+	/// 	None => println!("Key doesn't exist"),
+	/// }
+	/// ```
 	pub fn get(&mut self, key: String) -> Option<String> {
 		match self.storage.get(&key) {
 			None => None,
@@ -21,7 +43,31 @@ impl KvStore {
 		}
 	}
 
+	/// Remove a given key from the store
+	///
+	/// # Examples
+	///
+	/// ```
+	/// use kvs::KvStore;
+	///
+	/// let mut kvstore = KvStore::new();
+	/// kvstore.remove("test_key".to_string());
+	/// ```
 	pub fn remove(&mut self, key: String) {
 		self.storage.remove(&key);
+	}
+
+	/// Set a given key and value Strings in the store
+	///
+	/// # Examples
+	///
+	/// ```
+	/// use kvs::KvStore;
+	///
+	/// let mut kvstore = KvStore::new();
+	/// kvstore.set("test_key".to_string(), "test_value".to_string());
+	/// ```
+	pub fn set(&mut self, key: String, value: String) {
+		self.storage.insert(key, value);
 	}
 }
