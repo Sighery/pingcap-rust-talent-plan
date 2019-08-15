59302281e4fff3f93afdfe160799f8a76a0966ce
Sighery <sighery7@gmail.com>
2019-08-15 22:13:19 +0200

    Accept command line arguments
    This completes part 2 of project 1:
    https://github.com/pingcap/talent-plan/blob/master/rust/projects/project-1/project.md#part-2-accept-command-line-arguments
    
    This part implements clap to make a CLI interface, and makes all the
    cli_* tests pass.

--- project1/src/bin/kvs.rs
+++ project1/src/bin/kvs.rs
@@ -1,7 +1,26 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 extern crate kvs;
 use kvs::KvStore;
 
 
 fn main() {
-	println!("Hello, world!");
+	let mut kvstore = KvStore::new();
+
+	let yaml = load_yaml!("cli.yml");
+	let matches = App::from_yaml(yaml).get_matches();
+
+	if let Some(matches) = matches.subcommand_matches("get") {
+		kvstore.get(matches.value_of("KEY").unwrap().to_string());
+	} else if let Some(matches) = matches.subcommand_matches("rm") {
+		kvstore.get(matches.value_of("KEY").unwrap().to_string());
+	} else if let Some(matches) = matches.subcommand_matches("set") {
+		let key = matches.value_of("KEY").unwrap().to_string();
+		let value = matches.value_of("VALUE").unwrap().to_string();
+		kvstore.set(key, value);
+	} else {
+		panic!("Some subcommand must be used");
+	}
 }
