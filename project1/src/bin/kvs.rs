c165f4b9e36225b02fe61f00ca0cee58fcaca9aa
Sighery <sighery7@gmail.com>
2019-08-17 03:08:41 +0200

    Revert implementation of the kvs subcommands
    This is done so three tests in particular. Those tests check that get,
    rm, and set subcommands fail and return `unimplemented`. Seems like I
    was too eager and implemented them too early.

--- project1/src/bin/kvs.rs
+++ project1/src/bin/kvs.rs
@@ -2,26 +2,15 @@
 extern crate structopt;
 use structopt::StructOpt;
 
-extern crate kvs;
-use kvs::KvStore;
-
 mod cli;
 use cli::{KvsCli, KvsCommand};
 
 fn main() {
-	let mut kvstore = KvStore::new();
-
 	let opts = KvsCli::from_args();
 
 	match opts.commands {
-		KvsCommand::Get { key } => {
-			let copy_key = key.clone();
-			match kvstore.get(key) {
-				Some(value) => println!("Value for key {} is {}", copy_key, value),
-				None => println!("No key {} in the store", copy_key),
-			}
-		}
-		KvsCommand::Rm { key } => kvstore.remove(key),
-		KvsCommand::Set { key, value } => kvstore.set(key, value),
+		KvsCommand::Get { .. } => panic!("unimplemented"),
+		KvsCommand::Rm { .. } => panic!("unimplemented"),
+		KvsCommand::Set { .. } => panic!("unimplemented"),
 	};
 }
